86a6d982122f257658daf5e59a81de3aee752888
Alex Henry <adhe223@g.uky.edu>
2017-11-28 14:34:56 -0800

    Add additional port option

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const priceRoutes = require('./routes/priceRoutes');
 
-const port = process.env.PORT || 8080;
+const port = process.env.$PORT || process.env.port || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
